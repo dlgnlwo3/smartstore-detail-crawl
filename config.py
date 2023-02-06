527e30198b93c01d2b3687488db7fbcdb94cd354
dlgnlwo3 <dlgnlwo3@naver.com>
2023-02-06 17:56:05 +0900

    API 저장파일 json화

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 import os
-import shutil
+from enum import Enum
 from datetime import datetime
+import json
+from mimetypes import MimeTypes
+
 
 COMPANY_NAME = "consolework"
 PROGRAM_ID = "smartstore-detail-crawl"
 APP_DATA_PATH = os.path.join(os.getenv("APPDATA"), COMPANY_NAME)
-EXE_PATH = os.getcwd()
+PROGRAM_PATH = os.path.join(APP_DATA_PATH, PROGRAM_ID)
 
+
+# 프로그램이 실행되는 exe파일 경로
+EXE_PATH = os.getcwd()
 
 if os.path.isdir(APP_DATA_PATH) == False:
     os.mkdir(APP_DATA_PATH)
@@ -24,3 +30,57 @@ if not os.path.isdir(OUTPUT_FOLDER_NAME):
 TODAY_OUTPUT_FOLDER = os.path.join(os.getcwd(), OUTPUT_FOLDER_NAME, datetime.today().strftime("%Y%m%d"))
 if not os.path.isdir(TODAY_OUTPUT_FOLDER):
     os.mkdir(TODAY_OUTPUT_FOLDER)
+
+# ------------- 저장파일 경로 생성 종료 ------------- #
+API_SAVE_PATH = os.path.join(PROGRAM_PATH, f"{PROGRAM_ID}_api.txt")
+
+
+def get_api_save_data():
+    saved_data = None
+    if os.path.isfile(API_SAVE_PATH):
+        with open(API_SAVE_PATH, "r", encoding="utf-8") as f:
+            saved_data = json.loads(f.read())
+    return saved_data
+
+
+def write_api_save_data(dict_save: dict):
+
+    if os.path.isfile(API_SAVE_PATH):
+        os.remove(API_SAVE_PATH)
+        with open(API_SAVE_PATH, "w", encoding="utf-8") as f:
+            f.write(json.dumps((dict_save)))
+            f.close()
+    else:
+        with open(API_SAVE_PATH, "w", encoding="utf-8") as f:
+            f.write(json.dumps((dict_save)))
+            f.close()
+
+    return dict_save
+
+
+class SaveFile(Enum):
+    OPENAPI_CLIENT_ID = "OPENAPI_CLIENT_ID"
+    OPENAPI_CLIENT_SECRET = "OPENAPI_CLIENT_SECRET"
+    COMMERCEAPI_CLIENT_ID = "COMMERCEAPI_CLIENT_ID"
+    COMMERCEAPI_CLIENT_SECRET = "COMMERCEAPI_CLIENT_SECRET"
+
+
+# 저장데이터 없는 경우 초기화
+if not get_api_save_data():
+    write_api_save_data(
+        {
+            SaveFile.OPENAPI_CLIENT_ID.value: "",
+            SaveFile.OPENAPI_CLIENT_SECRET.value: "",
+            SaveFile.COMMERCEAPI_CLIENT_ID.value: "",
+            SaveFile.COMMERCEAPI_CLIENT_SECRET.value: "",
+        }
+    )
+# ------------- 저장파일 경로 생성 종료 ------------- #
+
+
+# ------------- 로그파일 경로 생성 시작 ------------- #
+LOG_FOLDER_NAME = "log"
+LOG_FOLDER_PATH = os.path.join(PROGRAM_PATH, LOG_FOLDER_NAME)
+if not os.path.isdir(LOG_FOLDER_PATH):
+    os.mkdir(LOG_FOLDER_PATH)
+# ------------- 로그파일 경로 생성 종료 ------------- #
--- tabs/API_setting_tab.py
+++ tabs/API_setting_tab.py
@@ -19,6 +19,10 @@ class APISettingUI(QWidget):
 
     # 초기화
     def __init__(self):
+        print(f"API_SAVE_PATH: {API_SAVE_PATH}")
+        self.saved_data = get_api_save_data()
+        print(self.saved_data)
+
         super().__init__()
         self.initUI()
 
@@ -30,142 +34,70 @@ class APISettingUI(QWidget):
         self.browser.append(f"[{now}] {str(text)}")
         global_log_append(text)
 
-    # 상품목록 시작 클릭
-    def product_list_search_start_button_clicked(self):
-        print(f"product list search start clicked")
+    # 상태 저장
+    def save_button_clicked(self):
 
-        if self.store_url.text() == "":
-            print(f"주소가 입력되지 않았습니다.")
-            self.log_append(f"주소가 입력되지 않았습니다.")
-            return
+        openAPI_client_id = self.openAPI_client_id.text()
+        openAPI_client_secret = self.openAPI_client_secret.text()
+        commerceAPI_client_id = self.commerceAPI_client_id.text()
+        commerceAPI_client_secret = self.commerceAPI_client_secret.text()
 
-        guiDto = GUIDto()
-        guiDto.store_url = self.store_url.text()
+        dict_save = {
+            SaveFile.OPENAPI_CLIENT_ID.value: openAPI_client_id,
+            SaveFile.OPENAPI_CLIENT_SECRET.value: openAPI_client_secret,
+            SaveFile.COMMERCEAPI_CLIENT_ID.value: commerceAPI_client_id,
+            SaveFile.COMMERCEAPI_CLIENT_SECRET.value: commerceAPI_client_secret,
+        }
 
-        print(f"상품목록 검색을 시작합니다. {guiDto.store_url}")
+        question_msg = "저장하시겠습니까?"
+        reply = QMessageBox.question(self, "상태 저장", question_msg, QMessageBox.Yes, QMessageBox.No)
 
-        self.product_list_search_thread = ProductListSearchThread()
-        self.product_list_search_thread.log_msg.connect(self.log_append)
-        self.product_list_search_thread.product_list_search_finished.connect(self.product_list_search_finished)
-        self.product_list_search_thread.setGuiDto(guiDto)
-
-        self.product_list_search_start_button.setDisabled(True)
-        self.product_list_search_stop_button.setDisabled(False)
-        self.product_list_search_thread.start()
-
-    # 상품목록 중지 클릭
-    @pyqtSlot()
-    def product_list_search_stop_button_clicked(self):
-        print(f"product list search stop clicked")
-        self.log_append(f"중지 클릭")
-        self.product_list_search_finished()
-
-    # 상품목록 작업 종료
-    @pyqtSlot()
-    def product_list_search_finished(self):
-        print(f"product list search thread finished")
-        self.log_append(f"검색 작업 종료")
-        self.product_list_search_thread.stop()
-        self.product_list_search_start_button.setDisabled(False)
-        self.product_list_search_stop_button.setDisabled(True)
-        print(f"thread_is_running: {self.product_list_search_thread.isRunning()}")
-
-    # 상품상세정보 시작 클릭
-    def product_detail_search_start_button_clicked(self):
-        print(f"search start clicked")
-
-        if self.product_list_excel_file.text() == "":
-            print(f"선택된 파일이 없습니다.")
-            self.log_append(f"선택된 파일이 없습니다.")
-            return
-
-        guiDto = GUIDto()
-        guiDto.product_list_excel_file = self.product_list_excel_file.text()
-
-        print(f"상품목록 검색을 시작합니다. {guiDto.product_list_excel_file}")
-
-        self.product_detail_search_thread = ProductDetailSearchThread()
-        self.product_detail_search_thread.log_msg.connect(self.log_append)
-        self.product_detail_search_thread.product_detail_search_finished.connect(self.product_detail_search_finished)
-        self.product_detail_search_thread.setGuiDto(guiDto)
-
-        self.product_detail_search_start_button.setDisabled(True)
-        self.product_detail_search_stop_button.setDisabled(False)
-        self.product_detail_search_thread.start()
-
-    # 상품상세정보 중지 클릭
-    @pyqtSlot()
-    def product_detail_search_stop_button_clicked(self):
-        print(f"search stop clicked")
-        self.log_append(f"중지 클릭")
-        self.product_detail_search_finished()
-
-    # 상품상세정보 작업 종료
-    @pyqtSlot()
-    def product_detail_search_finished(self):
-        print(f"search thread finished")
-        self.log_append(f"검색 작업 종료")
-        self.product_detail_search_thread.stop()
-        self.product_detail_search_start_button.setDisabled(False)
-        self.product_detail_search_stop_button.setDisabled(True)
-        print(f"thread_is_running: {self.product_detail_search_thread.isRunning()}")
-
-    # 엑셀 파일 선택
-    def product_list_excel_file_select_button_clicked(self):
-        print(f"excel file select")
-        file_name = QFileDialog.getOpenFileName(self, "", "", "excel file (*.xlsx)")
-
-        if file_name[0] == "":
-            print(f"선택된 파일이 없습니다.")
-            self.log_append(f"선택된 파일이 없습니다.")
-            return
-
-        print(file_name[0])
-        self.product_list_excel_file.setText(file_name[0])
+        if reply == QMessageBox.Yes:
+            write_api_save_data(dict_save)
+            print(f"현재 상태를 저장했습니다.")
+        else:
+            print(f"저장 취소")
 
     # 메인 UI
     def initUI(self):
 
         # 상품목록 그룹박스
-        product_list_groupbox = QGroupBox("상품목록 크롤링")
-        self.store_url_label = QLabel(f"상점주소")
-        self.store_url = QLineEdit("")
-        self.product_list_search_start_button = QPushButton("검색시작")
-        self.product_list_search_stop_button = QPushButton("중지")
-        self.product_list_search_stop_button.setDisabled(True)
+        openAPI_groupbox = QGroupBox("네이버 오픈 API")
+        self.openAPI_client_id_label = QLabel("API KEY")
+        self.openAPI_client_id = QLineEdit(f"{self.saved_data[SaveFile.OPENAPI_CLIENT_ID.value]}")
+        self.openAPI_client_secret_label = QLabel("API SECRET")
+        self.openAPI_client_secret = QLineEdit(f"{self.saved_data[SaveFile.OPENAPI_CLIENT_SECRET.value]}")
 
-        self.product_list_search_start_button.clicked.connect(self.product_list_search_start_button_clicked)
-        self.product_list_search_stop_button.clicked.connect(self.product_list_search_stop_button_clicked)
-
-        product_list_inner_layout = QHBoxLayout()
-        product_list_inner_layout.addWidget(self.store_url_label)
-        product_list_inner_layout.addWidget(self.store_url)
-        product_list_inner_layout.addWidget(self.product_list_search_start_button)
-        product_list_inner_layout.addWidget(self.product_list_search_stop_button)
-        product_list_groupbox.setLayout(product_list_inner_layout)
+        openAPI_inner_layout = QHBoxLayout()
+        openAPI_inner_layout.addWidget(self.openAPI_client_id_label)
+        openAPI_inner_layout.addWidget(self.openAPI_client_id)
+        openAPI_inner_layout.addWidget(self.openAPI_client_secret_label)
+        openAPI_inner_layout.addWidget(self.openAPI_client_secret)
+        openAPI_groupbox.setLayout(openAPI_inner_layout)
 
         # 상품상세정보 그룹박스
-        product_detail_groupbox = QGroupBox("상품 목록 설정")
-        self.product_list_excel_file_label = QLabel(f"엑셀 파일 선택")
-        self.product_list_excel_file = QLineEdit("")
-        self.product_list_excel_file.setDisabled(True)
-        self.product_list_excel_file.setPlaceholderText("엑셀 파일 선택")
-        self.product_list_excel_file_select_button = QPushButton("파일 선택")
-        self.product_detail_search_start_button = QPushButton("검색시작")
-        self.product_detail_search_stop_button = QPushButton("중지")
-        self.product_detail_search_stop_button.setDisabled(True)
+        commerceAPI_groupbox = QGroupBox("네이버 커머스 API")
+        self.commerceAPI_client_id_label = QLabel("API KEY")
+        self.commerceAPI_client_id = QLineEdit(f"{self.saved_data[SaveFile.COMMERCEAPI_CLIENT_ID.value]}")
+        self.commerceAPI_client_secret_label = QLabel("API SECRET")
+        self.commerceAPI_client_secret = QLineEdit(f"{self.saved_data[SaveFile.COMMERCEAPI_CLIENT_SECRET.value]}")
 
-        self.product_list_excel_file_select_button.clicked.connect(self.product_list_excel_file_select_button_clicked)
-        self.product_detail_search_start_button.clicked.connect(self.product_detail_search_start_button_clicked)
-        self.product_detail_search_stop_button.clicked.connect(self.product_detail_search_stop_button_clicked)
+        commerceAPI_inner_layout = QHBoxLayout()
+        commerceAPI_inner_layout.addWidget(self.commerceAPI_client_id_label)
+        commerceAPI_inner_layout.addWidget(self.commerceAPI_client_id)
+        commerceAPI_inner_layout.addWidget(self.commerceAPI_client_secret_label)
+        commerceAPI_inner_layout.addWidget(self.commerceAPI_client_secret)
+        commerceAPI_groupbox.setLayout(commerceAPI_inner_layout)
 
-        product_detail_inner_layout = QHBoxLayout()
-        product_detail_inner_layout.addWidget(self.product_list_excel_file_label)
-        product_detail_inner_layout.addWidget(self.product_list_excel_file)
-        product_detail_inner_layout.addWidget(self.product_list_excel_file_select_button)
-        product_detail_inner_layout.addWidget(self.product_detail_search_start_button)
-        product_detail_inner_layout.addWidget(self.product_detail_search_stop_button)
-        product_detail_groupbox.setLayout(product_detail_inner_layout)
+        # 상태 저장 그룹박스
+        save_groupbox = QGroupBox(f"작업 시작")
+        self.save_button = QPushButton("저장")
+
+        self.save_button.clicked.connect(self.save_button_clicked)
+
+        save_inner_layout = QHBoxLayout()
+        save_inner_layout.addWidget(self.save_button)
+        save_groupbox.setLayout(save_inner_layout)
 
         # 로그 그룹박스
         log_groupbox = QGroupBox("로그")
@@ -177,10 +109,12 @@ class APISettingUI(QWidget):
 
         # 레이아웃 배치
         top_layout = QVBoxLayout()
-        top_layout.addWidget(product_list_groupbox)
-        top_layout.addWidget(product_detail_groupbox)
+        top_layout.addWidget(openAPI_groupbox)
+        top_layout.addWidget(commerceAPI_groupbox)
 
         mid_layout = QHBoxLayout()
+        mid_layout.addStretch(8)
+        mid_layout.addWidget(save_groupbox, 2)
 
         log_layout = QVBoxLayout()
         log_layout.addWidget(log_groupbox)
